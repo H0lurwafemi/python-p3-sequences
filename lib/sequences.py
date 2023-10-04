3be924da0d158d10ae126d9ba902f11a0a523e39
unknown <stephenoluwafemi52@gmail.com>
2023-10-04 07:46:10 +0100

    Done with assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,13 @@
 
 def print_fibonacci(length):
     pass
+
+def print_fibonacci(length):
+    fib_sequence = []
+    a, b = 0, 1
+
+    while len(fib_sequence) < length:
+        fib_sequence.append(a)
+        a, b = b, a + b
+
+    print(fib_sequence)
